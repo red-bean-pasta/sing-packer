0abf15dcab0b51fd91de9215fa449272e593f9f0
red-bean-pasta <red-bean-pasta@outlook.com>
2026-05-27 19:36:31 +0800

    feat: add config validity period certification support; refactor executable url logic

--- linux_or_mac/launch.sh
+++ linux_or_mac/launch.sh
@@ -6,10 +6,13 @@ work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Main(){
     SourceEnv
 
-    executable="$work_dir/sing-box"
+    arch=$(GetArch)
+    kernel=$(GetKernel)
     version=${VERSION:-}
-    exe_url=${EXE_URL:-}
+    exe_url=$(GetExeUrl "$version" "$kernel" "$arch")
     EnsureVarSet version exe_url
+
+    executable="$work_dir/sing-box"
     EnsureExeExists
 
     config_url=${CONFIG_URL:-}
@@ -17,9 +20,11 @@ Main(){
     username=${USERNAME:-}
     password=${PASSWORD:-}
     key=${KEY:-}
+    duration=${DURATION:-}
+    EnsureVarSet config_url port username password key duration
+
     config="$work_dir/config.json"
-    EnsureVarSet config_url port username password key
-    DownloadConfig
+    EnsureConfig
 
     LaunchExe
 }
@@ -52,26 +57,19 @@ EnsureExeExists(){
     fi
 }
 
-DownloadConfig(){
-    local tmp; tmp="$(mktemp "${work_dir}/config.json.XXXXXX")"
-    local agent; agent=$(GetAgent)
-    Log "Curling config from $config_url at port $port with agent as '$agent' and version '$version'..."
-    curl \
-        --connect-timeout 10 \
-        --max-time 20 \
-        --silent \
-        --fail --show-error \
-        -u "$username:$password" \
-        --get "https://$config_url:$port/cfg" \
-        --data-urlencode "agent=$agent" \
-        --data-urlencode "version=$version" \
-        --data-urlencode "key=$key" \
-        -o "$tmp" || {
-			rm -f "$tmp"
-			return 1
-		}
-
-    mv "$tmp" "$config"
+EnsureConfig(){
+    if [[ ! -f "$config" ]]; then
+        Log "Config file not found. Initializing..."
+        DownloadConfig
+        return 0
+    fi
+    if CheckConfigExpired "$config" "$duration"; then
+        Log "Config file expired. Refreshing..."
+        DownloadConfig
+        return 0
+    fi
+    Log "Found valid config file"
+    return 0
 }
 
 LaunchExe(){
@@ -130,13 +128,68 @@ ExtractExe() {
 	fi
 }
 
-GetAgent(){
+CheckConfigExpired(){
+    local now; now=$(date +%s)
+    local mtime; mtime=$(stat -c %Y "$1")
+    local age; age=$(( now - mtime ))
+    (( age > $2 * 3600 ))
+}
+
+DownloadConfig(){
+    local tmp; tmp="$(mktemp "${work_dir}/config.json.XXXXXX")"
+    local agent; agent=$(GetAgent)
+    Log "Curling config from $config_url at port $port with agent as '$agent' and version '$version'..."
+    curl \
+        --connect-timeout 10 \
+        --max-time 20 \
+        --silent \
+        --fail --show-error \
+        -u "$username:$password" \
+        --get "https://$config_url:$port/cfg" \
+        --data-urlencode "agent=$agent" \
+        --data-urlencode "version=$version" \
+        --data-urlencode "key=$key" \
+        -o "$tmp" || {
+			rm -f "$tmp"
+			return 1
+		}
+
+    mv "$tmp" "$config"
+}
+
+GetArch(){
+    local arch; arch="$(uname -m)"
+    case "$arch" in
+		i386|i486|i586|i686)
+			echo "386" ;;
+        x86_64)
+            echo "amd64" ;;
+		aarch64)
+			echo "arm64" ;;
+        *)
+            echo "$arch" ;;
+    esac
+}
+
+GetKernel(){
     local os; os="$(uname -s)"
     case "$os" in
-    Linux*)
+		Linux*)
+			echo "linux" ;;
+		Darwin*)
+			echo "darwin" ;;
+		*)
+			echo "$os" ;;
+	esac
+}
+
+GetAgent(){
+    local os; os=${kernel:-$(GetKernel)}
+    case "$os" in
+    linux*)
         echo "linux"
         ;;
-    Darwin*)
+    darwin*)
         echo "macos"
         ;;
     *)
